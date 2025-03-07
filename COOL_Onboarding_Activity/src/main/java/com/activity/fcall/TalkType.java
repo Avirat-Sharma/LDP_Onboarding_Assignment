2186e649922b327c4b97fd3db9128a3254292235
Avirat Sharma <avirat.sharma@iongroup.com>
2025-03-07 16:24:05 +0530

    add TalkProperty and TalkType annotation in TalkType.java

--- COOL_Onboarding_Activity/src/main/java/com/activity/fcall/TalkType.java
+++ COOL_Onboarding_Activity/src/main/java/com/activity/fcall/TalkType.java
@@ -1,16 +1,17 @@
 package com.activity.fcall;
 
-import com.iontrading.isf.commons.async.AsyncResult;
-import com.iontrading.talk.api.annotation.TalkFunction;
-import com.iontrading.talk.api.annotation.TalkParam;
-import com.iontrading.talk.ionbus.spi.IonBusInfo;
+import com.iontrading.talk.api.annotation.Identifier;
+import com.iontrading.talk.api.annotation.TalkProperty;
+import com.iontrading.talk.api.annotation.TalkType;
 
+@TalkType(name="BondTalkType")
 public class TalkType {
     private final String className="TalkType";
 
     @TalkProperty(name="EnhancedBond") public EnhancedBondPriceBean enhancedBond;
     @TalkProperty(name="OriginalBond") public BondPriceBean originalBond;
 
+    @TalkProperty(name="ClassName")
     public String getClassName() {
         return className;
     }
